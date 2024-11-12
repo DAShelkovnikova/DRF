453f4dbf9c7a1836ea2947b850dca03d688877d8
Lotos <shelkovnikova.da@gmail.com>
2024-11-12 23:04:08 +0300

    1

---
+++ materials/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Название курса",
+        help_text="Укажите название курса",
+    )
+    picture = models.ImageField(
+        upload_to="materials/picture", blank=True, null=True, verbose_name="Фото курса"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание курса",
+        help_text="Укажите описание курса",
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    title = models.CharField(
+        max_length=3000,
+        verbose_name="Название урока",
+        help_text="Укажите название урока",
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        verbose_name="Курс",
+        help_text="Выберите курс",
+        blank=True,
+        null=True,
+    )
+    preview = models.ImageField(
+        upload_to="materials/picture", blank=True, null=True, verbose_name="Фото урока"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание урока",
+        help_text="Укажите описание урока",
+    )
+    video = models.CharField(
+        max_length=150,
+        verbose_name="Ссылка на видео",
+        help_text="Укажите ссылку на видео",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
